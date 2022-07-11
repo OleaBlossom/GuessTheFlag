054837f5b414eeaab093a17c189ba78dfccba995
olea <olea.plum@gmail.com>
2022-07-11 16:24:02 +0200

    add capsule shaped button with alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var showingAlert = false
 	var body: some View {
 		ZStack {
 			AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
 			
-			Text("Your content")
-				.foregroundStyle(.secondary)
-				.padding(50)
-				.background(.ultraThinMaterial)
+			Button("Show Alert") {
+				showingAlert = true
+			}
+			.padding(.vertical, 16)
+			.padding(.horizontal, 32)
+			.background(.thickMaterial)
+			.cornerRadius(40)
+			.overlay(
+				Capsule(style: .circular)
+					.stroke(.white, lineWidth: 3)
+			)
+			.alert("Important message", isPresented: $showingAlert) {
+				Button("OK", role: .cancel) { }
+			} message: {
+				Text("Please read this!")
+			}
 		}
-		.ignoresSafeArea()
 	}
 }
 
