7454fc96918356c5bb6c0ef3c3e6623d8d4c6916
olea <olea.plum@gmail.com>
2022-07-11 18:55:01 +0200

    add title and placeholder for score, make the UI pretty on most iPhone sizes

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,33 +10,62 @@ import SwiftUI
 struct ContentView: View {
 	@State private var showingScore = false
 	@State private var scoreTitle = ""
-	@State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+	@State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "the UK", "the US"]
 		.shuffled()
 	@State private var correctAnswer = Int.random(in: 0...2)
 	var body: some View {
 		ZStack {
-			LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
-				.ignoresSafeArea()
+			RadialGradient(stops: [
+				.init(color: Color(red: 1, green: 0.2, blue: 0.45, opacity: 0.8), location: 0.3),
+				.init(color: Color(red: 0.76, green: 0.15, blue: 1, opacity: 0.4), location: 0.3),
+			], center: .top, startRadius: 200, endRadius: 400)
+			.ignoresSafeArea()
 			
-			VStack(spacing: 30) {
-				VStack {
-					Text("Tap the flag of")
-						.font(.subheadline.weight(.heavy))
-						.foregroundColor(.white)
-					Text(countries[correctAnswer])
-						.font(.largeTitle.weight(.semibold))
-						.foregroundColor(.white)
-				}
+			VStack() {
+				Spacer()
+				Text("Guess the Flag")
+					.font(.largeTitle.bold())
+					.foregroundColor(.white)
+								
+				Text("Score: ???")
+					.foregroundColor(.white)
+					.font(.title.bold())
 				
-				ForEach(0..<3) { number in
-					Button {
-						flagTapped(number)
-					} label: {
-						Image(countries[number])
-							.renderingMode(.original)
+				Spacer()
+				Spacer()
+				
+				VStack(spacing: 15) {
+					VStack {
+						Text("Tap the flag of")
+							.font(.subheadline.weight(.heavy))
+							.foregroundColor(.secondary)
+						Text(countries[correctAnswer])
+							.font(.largeTitle.weight(.semibold))
+							.foregroundColor(.secondary)
+					}
+					
+					ForEach(0..<3) { number in
+						Button {
+							flagTapped(number)
+						} label: {
+							Image(countries[number])
+								.renderingMode(.original)
+								.clipShape(Capsule())
+								.overlay(
+									Capsule(style: .circular)
+										.stroke(.white, lineWidth: 4)
+								)
+						}
 					}
 				}
+				.frame(maxWidth: .infinity)
+				.padding(.vertical, 25)
+				.background(.ultraThinMaterial)
+				.clipShape(RoundedRectangle(cornerRadius: 20))
+				
+				Spacer()
 			}
+			.padding()
 		}
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
